44ed8d25c9e8b49348be65775820a7b5800749d1
A-fauzi <akhmadfauzi0101@gmail.com>
2022-08-09 15:13:37 +0700

    create pattern structure clean code

---
+++ src/utils/response.js
@@ -0,0 +1,15 @@
+module.exports = Response = () => {
+    const successMessage = (code, message, data) => ({
+        code : code,
+        message : message,
+        data : data
+    })
+    const errorMessage = (code, message) => ({
+        code : code,
+        message : message
+    })
+    return {
+        successMessage, errorMessage
+    }
+}
+
