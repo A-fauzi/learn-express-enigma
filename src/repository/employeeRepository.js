c67108079427e21f7086bbfe18d008f496238eec
A-fauzi <akhmadfauzi0101@gmail.com>
2022-08-09 15:24:14 +0700

    create file payload kedalam repository

--- src/repository/employeeRepository.js
+++ src/repository/employeeRepository.js
@@ -3,4 +3,18 @@ const employeeRepository = () => {
     const create = (payload) => {
         return employeeDB.push(payload)
     }
+
+    const list = () => {
+        return employeeDB
+    }
+
+    const getById = (id) => {
+        return employeeDB.find((e) => e.id === id)
+    }
+
+    return {
+        create, list, getById
+    }
 }
+
+module.exports = employeeRepository
