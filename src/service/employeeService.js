077c926d89f742ed088d9cfe9c74ce5ec21a27f3
A-fauzi <akhmadfauzi0101@gmail.com>
2022-08-09 16:38:05 +0700

    add business logic in employee service

---
+++ src/service/employeeService.js
@@ -0,0 +1,16 @@
+const employeeService = (employeeRepository) => {
+    const {create, list, getById} = employeeRepository()
+
+    const registerEmployee = (newEmployee) => {
+        return create(newEmployee)
+    }
+    const findAllEmployee = () => {
+        return list()
+    }
+    const findAllEmployeeById = (id) => {
+        getById(id)
+    }
+
+    return { registerEmployee, findAllEmployee, findAllEmployeeById }
+
+}
